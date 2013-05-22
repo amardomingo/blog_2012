60272ddfd5fa12f8756eb19e99247d0c3094de19
amardomingo <mardoalmar@gmail.com>
2013-05-22 23:20:09 +0200

    Arreglado mkconfig.sh

--- config/mkconfig.sh
+++ config/mkconfig.sh
@@ -4,11 +4,9 @@ cat > config.json <<EOF
 "username": "$DATABASE_USER",
 "password": "$DATABASE_PASSWORD",
 "database": "$DATABASE_NAME",
-"host":
-"$DATABASE_HOST",
+"host": "$DATABASE_HOST",
 "dialect": "$DATABASE_DIALECT",
-"port":
-"$DATABASE_PORT",
+"port": "$DATABASE_PORT",
 "protocol": "$DATABASE_PROTOCOL",
 "storage": "$DATABASE_STORAGE",
 "omitNull": true
